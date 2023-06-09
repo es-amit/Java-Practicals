67a7155c779288fd2d65973dd3ec173d21944b80
Amit Singh <101783688+es-amit@users.noreply.github.com>
2023-06-09 10:56:19 +0530

    difference between equals() and  ==

---
+++ Question-22/question_22.java
@@ -0,0 +1,8 @@
+public class question_22 {
+    public static void main(String[] args) {
+        String s1 = "Hello";
+        String s2 = new String(s1);
+        System.out.println(s1+" equals "+s2+" --> "+s1.equals(s2));
+        System.out.println(s1+" == "+s2+" --> "+(s1==s2));
+    }
+}
